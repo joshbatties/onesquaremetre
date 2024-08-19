c2e69d393c9ab38a3f367bf7045883672b4f025a
Joshua Batties <joshbatties.dev@gmail.com>
2024-08-19 17:22:03 +1000

    broke it lol

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import dynamic from 'next/dynamic';
+
+const DynamicNavbar = dynamic(() => import('../components/ui/Navbar'), {
+  ssr: false,
+});
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <DynamicNavbar />
+        {children}
+      </body>
     </html>
   );
 }
